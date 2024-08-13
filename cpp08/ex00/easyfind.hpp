5a6b12ddde649a85b25427e3306282f29600a931
esteiner <ellen@steiner-mercker.de>
2024-08-13 15:20:29 +0200

    started cpp08

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+
+template <typename T>
+typename T::iterator easyfind(T &src_container, int i)
+{
+    typename T::iterator iter = std::find(src_container.begin(), src_container.end(), i);
+    if (iter == src_container.end())
+    {
+        throw "Value not found in container";
+    }
+    return (iter);
+}
+
+#endif
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -10,3 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include "../easyfind.hpp"
+
+int main() {
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    try {
+        std::vector<int>::iterator it = easyfind(vec, 3);
+        std::cout << "Found value: " << *it << std::endl;
+    } catch (const char* e) {
+        std::cerr << e << std::endl;
+    }
+
+    try {
+        std::vector<int>::iterator it = easyfind(vec, 6);
+        std::cout << "Found value: " << *it << std::endl;
+    } catch (const char* e) {
+        std::cerr << e << std::endl;
+    }
+
+    return 0;
+}
